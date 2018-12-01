8f6ce6b650ea4f6c8cbe24b3927635463a441f49
hantsy <hantsy@gmail.com>
2018-12-01 21:17:17 +0800

    Added initial post service spec test.

--- client/src/app/post/shared/post.service.spec.ts
+++ client/src/app/post/shared/post.service.spec.ts
@@ -1,64 +1,105 @@
 /* tslint:disable:no-unused-variable */
-
+// Http testing module and mocking controller
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 import { TestBed, async, inject } from '@angular/core/testing';
-import { HttpModule, BaseRequestOptions, Http, Response, ResponseOptions } from '@angular/http';
-import { Observable} from 'rxjs/Rx';
+import { BaseRequestOptions, Http, Response, ResponseOptions } from '@angular/http';
+import { Observable} from 'rxjs';
 
-import { HttpInterceptorService } from '@covalent/http';
 import { PostService } from './post.service';
+import { Post } from './post.model';
 
 const posts = [
   {
-    id: 1,
+    id: '1',
     title: 'Getting started with REST',
     content: 'Content of Getting started with REST',
-    createdAt: '9/22/16 4:15 PM'
+    createdDate: '9/22/16 4:15 PM'
   },
   {
-    id: 2,
+    id: '2',
     title: 'Getting started with AngularJS 1.x',
     content: 'Content of Getting started with AngularJS 1.x',
-    createdAt: '9/22/16 4:15 PM'
+    createdDate: '9/22/16 4:15 PM'
   },
   {
-    id: 3,
-    title: 'Getting started with Angular2',
+    id: '3',
+    title: 'Getting started with Angular 2',
     content: 'Content of Getting started with Angular2',
-    createdAt: '9/22/16 4:15 PM'
+    createdDate: '9/22/16 4:15 PM'
   },
-];
-
-class HttpInterceptorServiceStub {
-  get(path) {
-    return Observable.of(posts);
-  }
-}
+] as Post[];
 
 describe('Service: Post', () => {
-  let HttpInterceptorService: HttpInterceptorService;
   let postService: PostService;
+
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpModule],
-      providers: [{ provide: HttpInterceptorService, useValue: new HttpInterceptorServiceStub() }, PostService]
+      // Import the HttpClient mocking services
+      imports: [ HttpClientTestingModule ],
+      // Provide the service-under-test and its dependencies
+      providers: [
+        PostService
+        // HttpErrorHandler,
+        // MessageService
+      ]
     });
+
+    // Inject the http, test controller, and service-under-test
+    // as they will be referenced by each test.
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+    postService = TestBed.get(PostService);
   });
 
-  beforeEach(inject([HttpInterceptorService, PostService], (_HttpInterceptorService, _postService) => {
-    HttpInterceptorService = _HttpInterceptorService;
-    postService = _postService;
-  }));
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
+
 
   it('should not be null...', () => {
     expect(postService).toBeTruthy();
   });
 
-  it('should get posts...', async(() => {
-    postService.getPosts()
-      .subscribe(res => {
-        expect(res).toEqual(posts);
-      });
-  }));
+  describe('#getPosts', () => {
+    let expectedPosts: Post[];
+
+    beforeEach(() => {
+       expectedPosts = posts;
+       postService = TestBed.get(PostService);
+    });
+
+    it('should get posts...', () => {
+      postService.getPosts()
+        .subscribe(
+          res => expect(res).toEqual(expectedPosts, 'should return exptected posts'),
+          fail
+        );
+
+      // PostService should have made one request to GET heroes from expected URL
+      const req = httpTestingController.expectOne(postService.apiUrl);
+      expect(req.request.method).toEqual('GET');
+
+      // Respond with the mock posts
+      req.flush(expectedPosts);
+
+    });
+
+    it('should be OK returning no posts', () => {
+
+      postService.getPosts()
+      .subscribe(
+        res => expect(res.length).toEqual(0, 'should have empty posts array'),
+        fail
+      );
+
+      const req = httpTestingController.expectOne(postService.apiUrl);
+      req.flush([]); // Respond with no posts
+    });
+  });
 
 });
--- client/src/app/post/shared/post.service.ts
+++ client/src/app/post/shared/post.service.ts
@@ -6,46 +6,49 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { HttpParams } from '@angular/common/http';
 
-const apiUrl = environment.baseApiUrl + '/posts';
-
 @Injectable()
 export class PostService {
-
+  apiUrl = environment.baseApiUrl + '/posts';
   constructor(private http: HttpClient) { }
 
   getPosts(term?: any): Observable<any> {
     const params: HttpParams = new HttpParams();
-    Object.keys(term).map(key => {
-      if (term[key]) { params.set(key, term[key]); }
-    });
-    return this.http.get(`${apiUrl}`, { params });
+    if (term) {
+      Object.keys(term)
+      .map(
+        key => {
+          if (term[key]) { params.set(key, term[key]); }
+        }
+    );
+  }
+    return this.http.get(`${this.apiUrl}`, { params });
   }
 
   getPost(id: string): Observable<any> {
-    return this.http.get(`${apiUrl}/${id}`);
+    return this.http.get(`${this.apiUrl}/${id}`);
   }
 
   savePost(data: Post) {
     console.log('saving post:' + data);
-    return this.http.post(`${apiUrl}`, data);
+    return this.http.post(`${this.apiUrl}`, data);
   }
 
   updatePost(id: string, data: Post) {
     console.log('updating post:' + data);
-    return this.http.put(`${apiUrl}/${id}`, data);
+    return this.http.put(`${this.apiUrl}/${id}`, data);
   }
 
   deletePost(id: string) {
     console.log('delete post by id:' + id);
-    return this.http.delete(`${apiUrl}/${id}`);
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
 
   saveComment(id: string, data: Comment) {
-    return this.http.post(`${apiUrl}/${id}/comments`, data);
+    return this.http.post(`${this.apiUrl}/${id}/comments`, data);
   }
 
   getCommentsOfPost(id: string): Observable<any> {
-    return this.http.get(`${apiUrl}/${id}/comments`);
+    return this.http.get(`${this.apiUrl}/${id}/comments`);
   }
 
 }
