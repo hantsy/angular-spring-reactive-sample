9ddf3ed994f2d43f93eba3c013cd8c94f65cd49d
hantsy <hantsy@gmail.com>
2023-04-01 22:05:57 +0800

    chore: clean codes

--- api/src/main/java/com/example/demo/interfaces/PostController.java
+++ api/src/main/java/com/example/demo/interfaces/PostController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 import java.net.URI;
 import java.util.Optional;
 
